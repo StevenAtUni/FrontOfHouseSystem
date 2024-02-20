8dc9bf00bb6eacb41b8f33ba46905327afd83f84
Hossain-Ahmed1 <126888433+Hossain-Ahmed1@users.noreply.github.com>
2024-02-20 16:04:18 +0000

    Update FOHManagementInterface.java
    added comments and spelling

--- src/FOHManagementInterface.java
+++ src/FOHManagementInterface.java
@@ -1,8 +1,12 @@
 public interface FOHManagementInterface {
-  public void getMenu (String[][] menu);
 
+  //Method to get the menu from mangement
+  public void getMenu (String[][] menu);
+  
+  //Get the booking day of the current day from management
   public void getBookingData (String bookingData);
 
-  public void sendRecipe(int bookingID, int bookingStartTime, int bookingEndTime, int diners, String[] dishesPurchased, int dishPrices, int tipAmount, int serviceCharge);
+  //send the receipts data to mangement
+  public void sendReceipts(int bookingID, int bookingStartTime, int bookingEndTime, int diners, String[] dishesPurchased, int dishPrices, int tipAmount, int serviceCharge);
 
 }
