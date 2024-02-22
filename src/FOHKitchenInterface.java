66484370c86a3acc2d3384c05d7b522fcb5dbe03
StxvxnsGithub <steven.chen@city.ac.uk>
2024-02-22 13:12:45 +0000

    Updating IAW requirements
    Edits after clarification edits I made to the requirements documents

--- src/FOHKitchenInterface.java
+++ src/FOHKitchenInterface.java
@@ -1,15 +1,8 @@
 public interface FOHKitchenInterface {
 
-    //  Dish ordering, a method that will send data of the order
-//  public void sendOrder(int orderID, int tableID, int[] dishes, String orderNote);
-
-    //Note update, the method on which will send an update to a note to the kitchen on a specific order
-//  public void noteUpdate (int orderID, String newOrderNote);
-
     // Method for the kitchen to indicate an order is complete
-    public void markOrderComplete(int orderID);
+    void markOrderComplete(int orderID);
 
     // Method for the kitchen to notify which dishes are unavailable
-    public void markItemUnavailable(int itemID);
-
+    void markItemUnavailable(int itemID);
 }
--- src/FOHManagementInterface.java
+++ src/FOHManagementInterface.java
@@ -1,12 +1,8 @@
 public interface FOHManagementInterface {
 
-    //Method to get the menu from mangement
-    public void getMenu (String[][] menu);
+    // Method for the management team to invoke and send us the new menu.
+    void sendMenu (Object[][] menu);
 
-    //Get the booking day of the current day from management
-    public void getBookingData (String bookingData);
-
-    //send the receipts data to mangement
-    public void sendReceipts(int bookingID, int bookingStartTime, int bookingEndTime, int diners, String[] dishesPurchased, int dishPrices, int tipAmount, int serviceCharge);
-
+    // Method for the management team to view how many tables (2 seats) are booked for specific time
+    int getAmountBooked(int timestamp);
 }
