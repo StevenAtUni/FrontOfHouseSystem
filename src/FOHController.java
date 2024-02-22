52d48e1b1d9b055902ad4c0bd89c6e81f9e4dc1e
StxvxnsGithub <steven.chen@city.ac.uk>
2024-02-22 13:13:42 +0000

    Updated IAW interface changes

--- src/FOHController.java
+++ src/FOHController.java
@@ -1,39 +1,19 @@
 public abstract class FOHController implements FOHManagementInterface, FOHKitchenInterface {
 
+    @Override
+    public void sendMenu(Object[][] menu) {
+    }
 
-  @Override
-  public void getMenu(String[][] menu) {
+    @Override
+    public int getAmountBooked(int timestamp) {
+        return 0;
+    }
 
-  }
+    @Override
+    public void markOrderComplete(int orderID) {
+    }
 
-  @Override
-  public void getBookingData(String bookingData) {
-
-  }
-
-  @Override
-  public void sendReceipts(int bookingID, int bookingStartTime, int bookingEndTime, int diners,
-      String[] dishesPurchased, int dishPrices, int tipAmount, int serviceCharge) {
-
-  }
-
-//  @Override
-//  public void sendOrder(int orderID, int tableID, int[] dishes, String orderNote) {
-//
-//  }
-
-//  @Override
-//  public void noteUpdate(int orderID, String newOrderNote) {
-//
-//  }
-
-  @Override
-  public void markOrderComplete(int orderID) {
-
-  }
-
-  @Override
-  public void markItemUnavailable(int itemID) {
-
-  }
+    @Override
+    public void markItemUnavailable(int itemID) {
+    }
 }
