7831ba873693d69310224eeec48b6c3909ba7c7e
StxvxnsGithub <steven.chen@city.ac.uk>
2024-02-23 10:08:16 +0000

    Rewriting comments

--- src/FOHKitchenInterface.java
+++ src/FOHKitchenInterface.java
@@ -1,8 +1,8 @@
 public interface FOHKitchenInterface {
 
-    // Method for the kitchen to indicate an order is complete
+    // Method for the kitchen to indicate an order is complete and appropriately notify for serving
     void markOrderComplete(int orderID);
 
-    // Method for the kitchen to notify which dishes are unavailable
+    // Method to notify the FoH System which menu items to display as no longer available to order
     void markItemUnavailable(int itemID);
 }
--- src/FOHManagementInterface.java
+++ src/FOHManagementInterface.java
@@ -3,9 +3,9 @@ public interface FOHManagementInterface {
     // Method for the management team to invoke and send us the new menu.
     void sendMenu (Object[][] menu);
 
-    // Method for the management team to view how many tables (2 seats) are booked for specific time
+    // Method to view how many individual 2-seat tables are booked for the specified time
     int getAmountBooked(int timestamp);
 
-    // Method for the management team to update the service charge percentage
+    // Method to update the service charge percentage. Integer used as per management's request
     void setServiceCharge(int newCharge);
 }
