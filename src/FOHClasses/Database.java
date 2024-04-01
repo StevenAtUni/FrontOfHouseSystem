652040c0e74ecaa8c0164ec687e5399f91f6de9d
Tahsinislam6 <Md.Islam.13@city.ac.uk>
2024-04-02 00:44:53 +0100

    Set up JDBC

---
+++ src/FOHClasses/Database.java
@@ -0,0 +1,26 @@
+package FOHClasses;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Database {
+
+    public static void  main(String[] args) {
+        Database project = new Database();
+        project.createConnection();
+
+    }
+
+    void createConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2033t07","in2033t07_d","qbB_pkC1GZQ");
+            System.out.println("Connection successful");
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE,null,ex);
+        }
+    }
+}
