5352da87b8229ecf02df23086b81a4df0aa442cf
Tahsinislam6 <127503823+Tahsinislam6@users.noreply.github.com>
2024-04-13 11:41:57 +0100

    Implemented DishDAO and MenuDAO

--- src/FOHClasses/DatabaseDAO/Database.java
+++ src/FOHClasses/DatabaseDAO/Database.java
@@ -1,13 +1,11 @@
 package FOHClasses.DatabaseDAO;
 
 import java.sql.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 public class Database {
 
     public static void  main(String[] args) {
-        BookingDAO.cancelConfirmedBooking(2);
 
     }
 
