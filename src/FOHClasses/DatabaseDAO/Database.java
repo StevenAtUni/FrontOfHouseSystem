e3e342d7d4655c6796623e49f973759052d8134b
Tahsinislam6 <127503823+Tahsinislam6@users.noreply.github.com>
2024-04-14 23:48:42 +0100

    Updated createBooking

--- src/FOHClasses/DatabaseDAO/Database.java
+++ src/FOHClasses/DatabaseDAO/Database.java
@@ -6,8 +6,12 @@ import java.sql.*;
 public class Database {
 
     public static void  main(String[] args) {
-        int[] tableIDs = {1, 5, 10};
-        BookingDAO.createBooking(4, "Martin", "12334521", 1713207600, 1713214800, tableIDs);
+        int[][] coversAndTables = {
+                {1, 1}, // Cover 1 at Table 1
+                {2, 1}, // Cover 2 at Table 1
+                {3, 1}, // Cover 3 at Table 1
+                {4, 1}  // Cover 4 at Table 1
+        };
     }
 
 
