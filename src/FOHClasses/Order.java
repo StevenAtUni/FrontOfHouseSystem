50ad8ddfe6f52b7f0ea348fbabeff9e5de0d0c52
kokobeen <josephhoness1@gmail.com>
2024-04-15 05:54:04 +0100

    removed redundant buttons and remove test data

--- src/FOHClasses/Order.java
+++ src/FOHClasses/Order.java
@@ -71,3 +71,5 @@ public class Order {
         return arr;
     }
 }
+
+
--- src/lancastersguiv2/Login.java
+++ src/lancastersguiv2/Login.java
@@ -6,6 +6,7 @@ package lancastersguiv2;
 
 
 import FOHClasses.DatabaseDAO.WaiterDAO;
+import FOHClasses.Terminal;
 
 import static FOHClasses.DatabaseDAO.WaiterDAO.checkPassword;
 
@@ -96,6 +97,9 @@ public class Login extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+
+        Terminal.initialise();
+
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -118,6 +122,7 @@ public class Login extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
